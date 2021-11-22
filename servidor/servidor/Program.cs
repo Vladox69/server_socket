a97a548975bf939ba43c3161abc292c36ff16d93
Vladox69 <marwladiagua2001@hotmail.com>
2021-11-22 12:01:20 -0500

    Proyecto creado

---
+++ servidor/servidor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = Properties.Resources.ip;
+            int puerto = int.Parse(Properties.Resources.port);
+            Server s = new Server(ip, puerto);
+            s.Start();
+        }
+    }
+}
