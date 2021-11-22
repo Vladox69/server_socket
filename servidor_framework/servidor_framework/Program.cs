3a2f6aa5f94957d3b905b31a8d672afe72ac0c66
Vladox69 <marwladiagua2001@hotmail.com>
2021-11-22 12:49:53 -0500

    Cambio a ,Net Framework

---
+++ servidor_framework/servidor_framework/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace servidor_framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = Properties.Settings.Default.ip;
+            int puerto = Properties.Settings.Default.port;
+            Servidor s = new Servidor(ip, puerto);
+            s.Start();
+        }
+    }
+}
